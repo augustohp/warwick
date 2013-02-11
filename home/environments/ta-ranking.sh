ebcff53305c0dcdc845038e069fef4fd3ee45bed
Augusto Pascutti <augusto@phpsp.org.br>
2013-02-11 15:23:23 -0200

    Environment configuration for ta-ranking project.

---
+++ home/environments/ta-ranking.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Environment variables for http://github.com/augustohp/taranking
+
+export RANKING_ENVIRONMENT="development"
+export RANKING_SALT="367764329430db34be92fd14a7a770ee"
+export RANKING_DB_HOST="127.0.0.1"
+export RANKING_DB_USER="root"
+export RANKING_DB_PASSWD=""
+export RANKING_DB_NAME="ranking"
+export RANKING_DB_DRIVER="pdo_mysql" 
