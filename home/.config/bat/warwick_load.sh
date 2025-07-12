ddec274dbf7a137187ddd45af4b4178666588c33
Augusto Pascutti <augusto.hp@gmail.com>
2025-07-11 22:12:43 -0300

    Moar batcat support

--- home/.config/bat/warwick_load.sh
+++ home/.config/bat/warwick_load.sh
@@ -1,7 +1,27 @@
 # home/.config/warwick/plugin/php/load.sh
-# PHP related configuration
+# BAT is a modern pager
+#
+#
 
-warwick_verbose "bat"
+BAT="$(command -v bat)"
+if [ -z "$BAT" ]
+then
+	return 0
+fi
+
+warwick_verbose "Bat cat"
 
 export BAT_THEME="Catppuccin Mocha"
+export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 
+alias bathelp='bat --plain --language=help'
+
+##
+# Will display `<command> --help` with syntax highlight
+# from "bat".
+##
+# Usage: help <command>
+help() {
+    "$@" --help 2>&1 \
+		| bathelp
+}
